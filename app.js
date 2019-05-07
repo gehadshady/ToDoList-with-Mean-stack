b514ee434e1385ab1e88d2b28496333c3acd707c
gehadshady <gehadkhaledshady1993@gmail.com>
2019-05-07 21:55:22 +0200

    change db connection

--- app.js
+++ app.js
@@ -11,6 +11,6 @@ app.listen(port,()=>{
 
 app.use(body_parser);
 
-mongoose.connect("mongodb+srv://gehad:123@todo-list-eyumt.mongodb.net/test?retryWrites=true",{ useNewUrlParser: true });
+mongoose.connect("mongodb+srv://gehad:123@todo-list-eyumt.mongodb.net/test?retryWrites=true");
 
 app.use("/api/user",userRouter);
